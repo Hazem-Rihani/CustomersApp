7aa6bf0d53194e16bb1527bd6ad6b456a5ee77ee
hazem-rihani <hazem.rihani@outlook.com>
2021-12-01 01:45:36 +0100

    api first commit

---
+++ CustomersApp.Api/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using CustomersApp.Api.DataAccess;
+using CustomersApp.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CustomersApp.Api.Controllers
+{  
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+     
+        private readonly ILogger<CustomersController> _logger;
+        private CustomerDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new CustomerDbContext();
+        }
+
+        ///Customers/id
+        [HttpGet]
+        public Customer Get(int id)
+        {
+            return _context.GetCustomer(id);
+        }
+
+        [HttpPost]
+        public Customer Add(Customer customer)
+        {
+            if (string.IsNullOrWhiteSpace(customer.Validate())) return _context.Create(customer);
+            return new Customer();
+        }
+
+        [HttpDelete]
+        public void Delete(int customerId)
+        {
+            _context.Remove(customerId);
+        }
+
+    }
+}
---
+++ CustomersApp.UnitTests/CustomerSeedData.cs
@@ -0,0 +1,41 @@
+﻿using CustomersApp.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApp.UnitTests
+{
+    internal static class CustomerSeedData
+    {
+        public static IEnumerable<Customer> Get => new List<Customer>
+        {
+            new Customer
+            {
+                Id = 1,
+                FirstName = "Hazem",
+                LastName = "Rihani",
+                Deleted = false,
+                CreatedDate = DateTime.Now
+            },
+            new Customer
+            {
+                Id = 2,
+                FirstName = "Stephanie",
+                LastName = "Hedoux",
+                Deleted = false,
+                CreatedDate = DateTime.Now
+            },
+            new Customer
+            {
+                Id = 3,
+                FirstName = "Christophe",
+                LastName = "Pillot",
+                Deleted = false,
+                CreatedDate = DateTime.Now
+            }
+        };
+
+    }
+}
